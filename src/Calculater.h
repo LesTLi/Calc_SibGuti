581cdd1a1dba44d50f5c139229ff79bad1301332
Ilya Gaydabura <ileavoin87@gmail.com>
2021-09-19 17:59:08 +0300

    Создание заголовочного файла

---
+++ src/Calculater.h
@@ -0,0 +1,20 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <math.h>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+
+private slots:
+private:
+};
+#endif
